85b44c3f75daade3652718c3cf531942b0e6a69c
alfiananandaid <alfianananda.id@gmail.com>
2026-08-31 20:05:14 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -3,7 +3,8 @@
  */
 const API = {
   async request(action, payload = {}) {
-    if (!CONFIG.API_URL || CONFIG.API_URL.includes("https://script.google.com/macros/s/AKfycbxnnQkAzNgPHwHkDFIgiWxYmZUkwKOTzebIVjKzdAvbpgXlYMN3EQJr_HQ7qWm4ZESqTQ/exec")) {
+    // Alarm ini hanya berbunyi jika URL masih mengandung teks "YOUR_APPS_SCRIPT"
+    if (!CONFIG.API_URL || CONFIG.API_URL.includes("YOUR_APPS_SCRIPT")) {
       throw new Error("URL API Google Apps Script belum dikonfigurasi di js/config.js!");
     }
 
@@ -21,7 +22,7 @@ const API = {
       const response = await fetch(CONFIG.API_URL, {
         method: 'POST',
         headers: {
-          'Content-Type': 'text/plain;charset=utf-8' // Apps Script CORS workaround
+          'Content-Type': 'text/plain;charset=utf-8'
         },
         body: JSON.stringify(fullPayload)
       });
