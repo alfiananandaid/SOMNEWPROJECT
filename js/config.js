8b9160345222481e378f950d8104956d8bbcb790
alfiananandaid <alfianananda.id@gmail.com>
2026-08-31 20:07:08 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+/**
+ * CONFIGURATION FILE
+ */
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbxnnQkAzNgPHwHkDFIgiWxYmZUkwKOTzebIVjKzdAvbpgXlYMN3EQJr_HQ7qWm4ZESqTQ/exec",
+  APP_NAME: "STOCK OPNAME MANDIRI",
+  VERSION: "1.0.0"
+};
